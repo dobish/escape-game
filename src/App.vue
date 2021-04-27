790a95114d03c56a9c12b6ef42d28359e9bfe112
maciekwoz <33222646+dobish@users.noreply.github.com>
2021-04-27 23:03:32 +0200

    Setup dev server https

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Yssssdaour Vue.js App"/>
+    <router-view class="main-view"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeScreen',
+  props: {
+
+  },
+      data: function(){
+        return {
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
